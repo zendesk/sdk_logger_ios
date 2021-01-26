cca10f4543c35515e7fc140e3e4b90bbd7cec992
Sarp Solakoglu <ssolakoglu@zendesk.com>
2021-01-26 16:39:16 +0000

    Update README, CONTRIBUTING and LICENSE

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+import PackageDescription
+let package = Package(
+    name: "ZendeskSDKLogger",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "ZendeskSDKLogger",
+            targets: [
+                "ZendeskSDKLogger"
+            ]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ZendeskSDKLogger",
+            path: "ZendeskSDKLogger.xcframework"
+        )
+    ]
+)
